84aea2c9b924506eeffb7e99eb90c3b3257771d9
Jonatandb <jonatandb@gmail.com>
2024-04-18 12:16:03 -0300

    Final cleaning

--- src/App.js
+++ src/App.js
@@ -6,20 +6,18 @@ function Square({ value, onSquareClick }) {
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)])
-  const [xIsNext, setXIsNext] = useState(true)
   const [currentMove, setCurrentMove] = useState(0)
   const currentSquares = history[currentMove]
+  const xIsNext = currentMove % 2 === 0
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove)
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
